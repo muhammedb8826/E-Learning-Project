aae8432ec0f00c510b27c7d29e7a512453169574
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-02-08 10:32:44 +0300

    Delete script folder

--- scripts/menu.js
+++
@@ -1,8 +0,0 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const nav = document.getElementById('nav');
-const mobileMenuBg = document.querySelector('.mobile-menu-bg');
-hamburgerMenu.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('change');
-  nav.classList.toggle('change');
-  mobileMenuBg.classList.toggle('change');
-});
